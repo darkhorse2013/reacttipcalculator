91b4d996ceb595c9f71dff3df747aa56fcbabefc
Samie Syed <samsonknight2k@hotmail.com>
2026-03-16 15:27:58 +0000

    put components into their own files and imported them into app.jsx

--- src/hooks/useExchangeRates.jsx
+++ src/hooks/useExchangeRates.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 //Use exchange rates api call
 export default function useExchangeRates() {
-  //make an api call and then persistently store in state.
+  //Make an api call and then persistently store in state.
+  //React re-renders components when state or props change.
+  //If stored in a local varaible, a re-render will not be triggered.
   //can't access app() states since that is localised, so must be declared here.
   //ui components can access app() state via props
   const [exchangeRates, setExchangeRates] = useState(null);
